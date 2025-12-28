bba44f2432e91bb1ab58f7d78a736b62f94f1131
benilias <benbrahimilias@gmail.com>
2025-12-28 22:46:03 +0000

    detect left/right hand from uploaded image

--- Frontend/src/utilities.jsx
+++ Frontend/src/utilities.jsx
@@ -2,9 +2,7 @@
 const labelMap = {
     1:{name:'Hello', color:'red'},
     2:{name:'Thank You', color:'yellow'},
-    3:{name:'I Love You', color:'lime'},
-    4:{name:'Yes', color:'blue'},
-    5:{name:'No', color:'purple'},
+    
 }
 
 // Define a drawing function
